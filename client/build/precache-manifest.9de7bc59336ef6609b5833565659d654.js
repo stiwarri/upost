ba6996dffc892c68c907ece55cbfb668d4c70b19
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-07-02 03:30:26 +0530

    Deployment: Add UI build files

---
+++ client/build/precache-manifest.9de7bc59336ef6609b5833565659d654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7ef326671df0bf38a9a",
+    "url": "/static/css/main.29328604.chunk.css"
+  },
+  {
+    "revision": "e7ef326671df0bf38a9a",
+    "url": "/static/js/main.e7ef3266.chunk.js"
+  },
+  {
+    "revision": "0576188fda4e09e1a308",
+    "url": "/static/js/1.0576188f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae788c35f62a1ea331e8c4cce913cc21",
+    "url": "/index.html"
+  }
+];
