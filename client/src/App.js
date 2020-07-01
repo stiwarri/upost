6b1feade671b92923252946b22cb1b0d39dfe496
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-07-02 02:18:38 +0530

    App Improvements: Change style

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import Particles from 'react-particles-js';
 
 import Layout from './components/Layout/Layout';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -238,7 +239,132 @@ class App extends Component {
                     }
                 />
                 {routes}
+                {
+                    !this.state.isAuth ?
+                        <Particles
+                            params={{
+                                particles: {
+                                    number: {
+                                        value: 100,
+                                        density: {
+                                            enable: true,
+                                            value_area: 800
+                                        }
+                                    },
+                                    color: { value: "#a3586d" },
+                                    shape: {
+                                        type: "circle",
+                                        stroke: {
+                                            width: 0,
+                                            color: "#a3586d"
+                                        },
+                                        polygon: {
+                                            nb_sides: 3
+                                        },
+                                        image: {
+                                            src: "img/github.svg",
+                                            width: 100,
+                                            height: 100
+                                        }
+                                    },
+                                    opacity: {
+                                        value: 0.5,
+                                        random: false,
+                                        anim: {
+                                            enable: false,
+                                            speed: 1,
+                                            opacity_min: 0.1,
+                                            sync: false
+                                        }
+                                    },
+                                    size: {
+                                        value: 5,
+                                        random: true,
+                                        anim: {
+                                            enable: false,
+                                            speed: 20,
+                                            size_min: 0.1,
+                                            sync: false
+                                        }
+                                    },
+                                    line_linked: {
+                                        enable: true,
+                                        distance: 130,
+                                        color: "#a3586d",
+                                        opacity: 0.4,
+                                        width: 1
+                                    },
+                                    move: {
+                                        enable: true,
+                                        speed: 6,
+                                        direction: "none",
+                                        random: false,
+                                        straight: false,
+                                        out_mode: "out",
+                                        bounce: false,
+                                        attract: {
+                                            enable: false,
+                                            rotateX: 600,
+                                            rotateY: 1200
+                                        }
+                                    }
+                                },
+                                interactivity: {
+                                    detect_on: "canvas",
+                                    events: {
+                                        onhover: {
+                                            enable: true,
+                                            mode: "repulse"
+                                        },
+                                        onclick: {
+                                            enable: true,
+                                            mode: "push"
+                                        },
+                                        resize: true
+                                    },
+                                    modes: {
+                                        grab: {
+                                            distance: 400,
+                                            line_linked: {
+                                                opacity: 1
+                                            }
+                                        },
+                                        bubble: {
+                                            distance: 400,
+                                            size: 40,
+                                            duration: 2,
+                                            opacity: 8,
+                                            speed: 3
+                                        },
+                                        repulse: {
+                                            distance: 100,
+                                            duration: 0.4
+                                        },
+                                        push: {
+                                            particles_nb: 4
+                                        },
+                                        remove: {
+                                            particles_nb: 2
+                                        }
+                                    }
+                                },
+                                retina_detect: true
+                            }
+                            }
+                            style={{
+                                width: '100%',
+                                backgroundColor: '#1f1641',
+                                height: '100vh',
+                                position: 'fixed',
+                                zIndex: '-1',
+                                top: '0',
+                                left: '0'
+                            }}
+                        />
+                        : null
+                }
             </Fragment>
+
         );
     }
 }
--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './Logo.css';
 
-const logo = props => <h1 className="logo">Node-App-Client</h1>;
+const logo = props => <h1 className="logo">uPost</h1>;
 
 export default logo;
